d3a96361ce4927b70108c56bb514dd09ea29edbc
Justin_Kinsey <justinkinsey95@gmail.com>
2025-12-02 12:42:17 -0600

    implemented changes from review

--- logic.py
+++ logic.py
@@ -44,7 +44,7 @@ class Logic(QMainWindow, Ui_MainWindow):
         When the user submits the number of tests and it is 1-4, the corresponding fields show.
         When the lineEdit_scores field is empty, the test fields are hidden.
         """
-        #TODO: lots of repetitive checks for num_tests checking to reveal fields.
+
         if self.lineEdit_scores.text().strip() == '':
             self.hide_fields()
 
@@ -86,6 +86,23 @@ class Logic(QMainWindow, Ui_MainWindow):
             self.lineEdit_test4.show()
             self.lineEdit_test1.setFocus()
 
+    def calc_grade(self, test1, test2, test3, test4, num_of_tests) -> str:
+        """
+        Receives 4 test scores as floats and determines the letter grade to return
+        :return: Letter grade as a string (A-F)
+        """
+        percent_grade = (test1 + test2 + test3 + test4) / num_of_tests
+        if percent_grade >= 90:
+            return 'A'
+        elif percent_grade >= 80:
+            return 'B'
+        elif percent_grade >= 70:
+            return 'C'
+        elif percent_grade >= 60:
+            return 'D'
+        else:
+            return 'F'
+
     def submit(self) -> None:
         """
         Sends student name (or anonymous) with the corresponding tests score(s) to a csv.
@@ -105,13 +122,15 @@ class Logic(QMainWindow, Ui_MainWindow):
             num_tests = int(self.lineEdit_scores.text())
             if num_tests <= 0 or num_tests > 4:
                 QMessageBox.warning(self, "Input Error", "Please enter a valid number of tests (1-4)")
+                return
         except ValueError:
             QMessageBox.warning(self, "Input Error", "Please enter valid information for the number of scores")
+            return
 
         #check test scores from revealed fields
         if len(self.lineEdit_test1.text()) > 0:
             try:
-                test1 = int(self.lineEdit_test1.text())
+                test1 = float(self.lineEdit_test1.text())
                 if test1 < 0 or test1 > 100:
                     QMessageBox.warning(self, "Input Error", "Please enter a valid score for test 1")
                     return
@@ -122,7 +141,7 @@ class Logic(QMainWindow, Ui_MainWindow):
 
         if len(self.lineEdit_test2.text()) > 0:
             try:
-                test2 = int(self.lineEdit_test2.text())
+                test2 = float(self.lineEdit_test2.text())
                 if test2 < 0 or test2 > 100:
                     QMessageBox.warning(self, "Input Error", "Please enter a valid score for test 2")
                     return
@@ -133,7 +152,7 @@ class Logic(QMainWindow, Ui_MainWindow):
 
         if len(self.lineEdit_test3.text()) > 0:
             try:
-                test3 = int(self.lineEdit_test3.text())
+                test3 = float(self.lineEdit_test3.text())
                 if test3 < 0 or test3 > 100:
                     QMessageBox.warning(self, "Input Error", "Please enter a valid score for test 3")
                     return
@@ -144,7 +163,7 @@ class Logic(QMainWindow, Ui_MainWindow):
 
         if len(self.lineEdit_test4.text()) > 0:
             try:
-                test4 = int(self.lineEdit_test4.text())
+                test4 = float(self.lineEdit_test4.text())
                 if test4 < 0 or test4 > 100:
                     QMessageBox.warning(self, "Input Error", "Please enter a valid score for test 4")
                     return
@@ -157,41 +176,49 @@ class Logic(QMainWindow, Ui_MainWindow):
         while not os.path.isfile('student_grades.csv'):
             with open('student_grades.csv', 'a', newline='') as csvfile:
                 content = csv.writer(csvfile)
-                content.writerow(['student', 'test 1', 'test 2', 'test 3', 'test 4', 'highest'])
+                content.writerow(['student', 'test 1', 'test 2', 'test 3', 'test 4', 'highest', 'Grade'])
                 break
 
         #updates the csv file
         with open('student_grades.csv', 'a', newline='') as csvfile:
             content = csv.writer(csvfile)
-            test1 = 0
-            test2 = 0
-            test3 = 0
-            test4 = 0
+            test1 = 0.0
+            test2 = 0.0
+            test3 = 0.0
+            test4 = 0.0
             #test1
             if len(self.lineEdit_test1.text()) == 0:
-                test1 = 0
+                test1 = 0.0
             else:
-                test1 = int(self.lineEdit_test1.text())
+                test1 = float(self.lineEdit_test1.text())
 
             #test2
             if len(self.lineEdit_test2.text()) == 0:
-                test2 = 0
+                test2 = 0.0
             else:
-                test2 = int(self.lineEdit_test2.text())
+                test2 = float(self.lineEdit_test2.text())
 
             #test3
             if len(self.lineEdit_test3.text()) == 0:
-                test3 = 0
+                test3 = 0.0
             else:
-                test3 = int(self.lineEdit_test3.text())
+                test3 = float(self.lineEdit_test3.text())
 
             # test4
             if len(self.lineEdit_test4.text()) == 0:
-                test4 = 0
+                test4 = 0.0
             else:
-                test4 = int(self.lineEdit_test4.text())
+                test4 = float(self.lineEdit_test4.text())
+            num_tests = int(self.lineEdit_scores.text())
             scores = [test1, test2, test3, test4]
             highest = max(scores)
-            content.writerow([student_name, test1, test2, test3, test4, highest])
+            grade = self.calc_grade(test1, test2, test3, test4, num_tests)
+            content.writerow([student_name, test1, test2, test3, test4, highest, grade])
+
+            #signals successful submission and clears fields
             self.label_submitted.setStyleSheet('color: green')
+            self.label_submitted.show()
+            self.lineEdit_scores.clear()
+            self.lineEdit_name.clear()
+            self.hide_fields()
             self.label_submitted.show()
